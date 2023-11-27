00f8ac3caae6f60a643bff45b37c3b974810f5db
aryan <aryan@vt.edu>
2023-11-27 14:57:10 -0500

    implemented features resume parser and editor and cover letter generator

--- app.py
+++ app.py
@@ -49,92 +49,166 @@ def calculate_match(resume_keywords, job_desc_keywords):
     match_percentage = len(match_keywords) / total_keywords * 100
     return match_percentage, match_keywords
 
-st.markdown("""
-# 📝 AI-Powered Cover Letter Generator
+st.markdown("# 📝 MyCoverKraft - Your Personalized Cover Letter Generator")
 
-Generate a cover letter. All you need to do is:
-1. Upload your resume or copy your resume/experiences
-2. Paste a relevant job description
-3. Input some other relevant user/job data
-"""
-)
+tab1, tab2 = st.tabs(["Resume Parser and Editor", "Cover Letter Generator"])
 
-# radio for upload or copy paste option         
-res_format = st.radio(
-    "Do you want to upload or paste your resume/key experience",
-    ('Upload', 'Paste'))
 
-if res_format == 'Upload':
-    # upload_resume
-        res_file = st.file_uploader('📁 Upload your resume in pdf format')
-        if res_file:
-            pdf_reader = PdfReader(res_file)
+if 'cover_letter_generated' not in st.session_state:
+    st.session_state.cover_letter_generated = False
+if 'feedback_submitted' not in st.session_state:
+    st.session_state.feedback_submitted = False
 
-            # Collect text from pdf
-            res_text = ""
-            for page in pdf_reader.pages:
-                res_text += page.extract_text()
-else:
-    res_text = st.text_input('Pasted resume elements')
+with tab1:
+    st.title("Resume Parser and Editor")
 
-with st.form('input_form'):
-    # other inputs
-    job_desc = st.text_input('Pasted job description')
-    user_name = st.text_input('Your name')
-    company = st.text_input('Company name')
-    manager = st.text_input('Hiring manager')
-    role = st.text_input('Job title/role')
-    referral = st.text_input('How did you find out about this opportunity?')
-    ai_temp = st.number_input('AI Temperature (0.0-1.0) Input how creative the API can be',value=.99)
+    with st.expander("Instructions"):
+        st.write("""
+            - Fields marked with an asterisk (*) are mandatory.
+            - Upload your resume 
+            - Edit your resume 
+            - Copy paste this resume in the cover letter generator
+        """)
 
-    # submit button
-    submitted = st.form_submit_button("Generate Cover Letter")
+    # Upload PDF resume
+    uploaded_file = st.file_uploader("Upload your resume* (PDF format)", type="pdf")
 
-# if the form is submitted run the openai completion   
-if submitted:
+    if uploaded_file:
+        # Extract text from the uploaded PDF file
+        extracted_text = extract_text_from_pdf(uploaded_file)
 
-    # note that the ChatCompletion is used as it was found to be more effective to produce good results
-    # using just Completion often resulted in exceeding token limits
-    # according to https://platform.openai.com/docs/models/gpt-3-5
-    # Our most capable and cost effective model in the GPT-3.5 family is gpt-3.5-turbo which has been optimized for chat 
-    # but works well for traditional completions tasks as well.
-    completion = ai.ChatCompletion.create(
-    #model="gpt-3.5-turbo-16k", 
-    model = "gpt-3.5-turbo",
-    temperature=ai_temp,
-    messages = [
-        {"role": "user", "content" : f"You will need to generate a cover letter based on specific resume and a job description"},
-        {"role": "user", "content" : f"My resume text: {res_text}"},
-        {"role": "user", "content" : f"The job description is: {job_desc}"},
-        {"role": "user", "content" : f"The candidate's name to include on the cover letter: {user_name}"},
-        {"role": "user", "content" : f"The job title/role : {role}"},
-        {"role": "user", "content" : f"The hiring manager is: {manager}"},
-        {"role": "user", "content" : f"How you heard about the opportunity: {referral}"},
-        {"role": "user", "content" : f"The company to which you are generating the cover letter for: {company}"},
-        {"role": "user", "content" : f"The cover letter should have three content paragraphs"},
-        {"role": "user", "content" : f""" 
-        In the first paragraph focus on the following: you will convey who you are, what position you are interested in, and where you heard
-        about it, and summarize what you have to offer based on the above resume
-        """},
-            {"role": "user", "content" : f""" 
-        In the second paragraph focus on why the candidate is a great fit drawing parallels between the experience included in the resume 
-        and the qualifications on the job description.
-        """},
+        # Display the extracted text in an editable text area
+        st.subheader("Extracted Resume Text")
+        st.write("Review and edit the extracted text from your resume:")
+        editable_text = st.text_area("Edit the text as needed:", extracted_text, height=300)
+
+        # Button to confirm the edited text
+        if st.button("Confirm Edited Text"):
+            st.session_state.edited_resume_text = editable_text
+            st.success("Resume text updated!")
+
+with tab2:
+    st.markdown("## Cover Letter Generator")
+    with st.expander("Instructions"):
+        st.write("""
+                    - Fields marked with an asterisk (*) are mandatory.
+                    - Upload your resume or copy your resume/experiences.
+                    - Paste a relevant job description.
+                    - Input other relevant data.
+                    - Choose a cover letter style.
+                """)
+    # radio for upload or copy paste option
+    res_format = st.radio(
+        "Resume Input Method",
+        ('Upload', 'Paste'),
+        help="Choose how you'd like to input your resume.")
+
+    with st.container():
+        if res_format == 'Upload':
+                res_file = st.file_uploader('📁 Upload your resume in pdf format')
+                if res_file:
+                    try:
+                        pdf_reader = PdfReader(res_file)
+                        res_text = ""
+                        for page in pdf_reader.pages:
+                            res_text += page.extract_text()
+                    except Exception as e:
+                        st.error("Error reading PDF file: " + str(e))
+
+        else:
+            res_text = st.text_input('Pasted resume elements')
+
+
+        st.info(
+            "Your data privacy is important. Uploaded resumes are only used for generating the cover letter and are not stored or used for any other purposes.")
+
+        # Tone adjustment
+        tone = st.selectbox('Select the Tone of Your Cover Letter',
+                            ['Professional', 'Friendly', 'Enthusiastic', 'Formal', 'Casual'],
+                            help="Choose the tone that best suits the company culture or job role.")
+
+        # Including specific achievements or skills
+        achievements = st.text_area('Include Specific Achievements, Skills or Keywords',
+                                    help="Mention any key achievements or skills that make you a strong candidate for the role.")
+
+        # Different letter structures
+        letter_structure = st.radio('Choose Your Cover Letter Structure',
+                                    ('Standard', 'Skill-based', 'Story-telling'),
+                                    help="Select a structure that aligns with how you want to present your information.")
+
+        with st.form('input_form'):
+            job_desc = st.text_input('Job description*')
+            user_name = st.text_input('Name*')
+            company = st.text_input('Company name*')
+            manager = st.text_input('Hiring manager')
+            role = st.text_input('Job Role*')
+            referral = st.text_input('How did you find out about this opportunity?')
+
+            submitted = st.form_submit_button("Generate Cover Letter")
+
+
+    if submitted and res_text and job_desc and user_name and company and role:
+        try:
+
+            customization_prompt = f"""
+            Tone: {tone.lower()}
+            Achievements/Skills: {achievements}
+            Structure: {letter_structure.lower()}
+            """
+            completion = ai.ChatCompletion.create(
+            model = "gpt-3.5-turbo",
+            temperature=0.99,
+            messages = [
+                {"role": "user", "content" : f"You will need to generate a cover letter based on specific resume and a job description"},
+                {"role": "user", "content" : f"My resume text: {res_text}"},
+                {"role": "user", "content" : f"The job description is: {job_desc}"},
+                {"role": "user", "content" : f"The candidate's name to include on the cover letter: {user_name}"},
+                {"role": "user", "content" : f"The job title/role : {role}"},
+                {"role": "user", "content" : f"The hiring manager is: {manager}"},
+                {"role": "user", "content" : f"How you heard about the opportunity: {referral}"},
+                {"role": "user", "content" : f"The company to which you are generating the cover letter for: {company}"},
+                {"role": "user", "content": customization_prompt},
+                {"role": "user", "content" : f"The cover letter should have three content paragraphs"},
                 {"role": "user", "content" : f""" 
-        In the 3RD PARAGRAPH: Conclusion
-      Restate your interest in the organization and/or job and summarize what you have to offer and thank the reader for their time and consideration.
-        """},
-        {"role": "user", "content" : f""" 
-        note that contact information may be found in the included resume text and use and/or summarize specific resume context for the letter
-            """},
-        {"role": "user", "content" : f"Use {user_name} as the candidate"},
-        
-        {"role": "user", "content" : f"Generate a specific cover letter based on the above. Generate the response and include appropriate spacing between the paragraph text"}
-    ]
-    )
-    #st.write(completion)
-    response_out = completion['choices'][0]['message']['content']
-    st.write(response_out)
+                In the first paragraph focus on the following: you will convey who you are, what position you are interested in, and where you heard
+                about it, and summarize what you have to offer based on the above resume
+                """},
+                    {"role": "user", "content" : f""" 
+                In the second paragraph focus on why the candidate is a great fit drawing parallels between the experience included in the resume 
+                and the qualifications on the job description.
+                """},
+                        {"role": "user", "content" : f""" 
+                In the 3RD PARAGRAPH: Conclusion
+              Restate your interest in the organization and/or job and summarize what you have to offer and thank the reader for their time and consideration.
+                """},
+                {"role": "user", "content" : f""" 
+                note that contact information may be found in the included resume text and use and/or summarize specific resume context for the letter
+                    """},
+                {"role": "user", "content" : f"Use {user_name} as the candidate"},
 
-    # include an option to download a txt file
-    st.download_button('Download the cover_letter', response_out)
+                {"role": "user", "content" : f"Generate a specific cover letter based on the above. Generate the response and include appropriate spacing between the paragraph text"}
+            ]
+            )
+
+            response_out = completion['choices'][0]['message']['content']
+            st.write(response_out)
+
+            st.download_button(
+                label="Download Cover Letter",
+                data=response_out,
+                file_name=user_name + "_cover_letter.txt",
+                mime="text/plain"
+            )
+
+            st.session_state.cover_letter_generated = True
+        except Exception as e:
+            logging.error("Error in cover letter generation: " + str(e))
+            st.error("An error occurred while generating the cover letter.")
+
+
+    if st.session_state.cover_letter_generated and not st.session_state.feedback_submitted:
+        feedback = st.slider("Rate the quality of the generated cover letter (1-5)", 1, 5, 3)
+        if st.button("Submit Feedback"):
+            save_feedback_to_file(user_name, feedback)
+            st.success("Thank you for your feedback!")
+            st.session_state.feedback_submitted = True
